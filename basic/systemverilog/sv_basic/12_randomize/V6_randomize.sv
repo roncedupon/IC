173375aca3b96c8e79c82214528ce71bc73f339b
roncedupon <2507317801@qq.com>
2025-05-03 09:36:12 +0000

    updtae

--- basic/systemverilog/sv_basic/12_randomize/V6_randomize.sv
+++ basic/systemverilog/sv_basic/12_randomize/V6_randomize.sv
@@ -3,18 +3,33 @@ class test;
     task test_tsk();
        repeat(10)begin
             randomize(data)with{
-                data>'h123;
-                data<'h256;
+                data>'d123;
+                data<'d256;
+                data inside{[135:211]};
             };
             $display("data randomize is %d",data);
         end
     endtask
 endclass
+class trasnsation;
+    rand bit[1:0]data1;
+    rand bit[1:0]data2;
+    task display();
+        $display("data1 is %0d",data1);
+        $display("data2 is %0d",data2);
+    endtask
+endclass
 module top;
     initial begin
         test test_inst;
+        trasnsation tr;
+        tr=new();
         test_inst=new();
         test_inst.test_tsk();
         $display("end %d",123);
+        tr.randomize()with{
+            data1==2'b11 && data2==2'b11 || data1==2'b01 && data2==2'b01 ;
+        };
+        tr.display();
     end
 endmodule
