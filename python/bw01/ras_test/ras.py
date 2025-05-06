327e2bb57ce1d89c8eca0a10efad25d2fc8c45a8
roncedupon <2507317801@qq.com>
2025-05-06 02:38:23 +0000

    增加ras的设计

--- python/bw01/ras_test/ras.py
+++ python/bw01/ras_test/ras.py
@@ -1,18 +1,26 @@
 import numpy as np
 class ras:
-    def __init__(self):
+    def __init__(self,weight_matrix):
         self.ras_col=16
         self.array_row=4096
         self.g=np.random.randint(1,3,(1,256))
         self.scale=None
+        self.weight_matrix=weight_matrix
         self.ras=np.zeros((self.array_row,self.ras_col))
+        self.ST =np.zeros((1,16)).squeeze()
         
-        
-    def calculate_ras(self,weight_matrix):
+    def calculate_ras(self,weight_matrix=None):
+        if weight_matrix==None:
+            weight_matrix=self.weight_matrix
         parts = np.hsplit(weight_matrix, self.ras_col)#return a list with length=ras_col 
         for col_index,item in enumerate(parts):
             print((item/self.g).sum(axis=1))
             self.ras[:,col_index]=item.sum(axis=1)
+            self.ST[col_index]=(item/self.g).sum()
+        print("\n===================================================")
+        print("RAS ERROR IS \n",self.calculate_error(weight_matrix))
+        print("===================================================\n")
+        return ras
             
     def int8_quantize(self,ras=None):
         if ras==None:
@@ -21,19 +29,26 @@ class ras:
         self.scale=2**np.round(np.log2(scale))
         ras=np.round(ras/scale)
         return ras
-
-    def dequantize(self,ras_quanted):
+    def dequantize(self,ras_quanted=None):
+        if ras_quanted==None:
+            ras_quanted=self.int8_quantize()
         #ras整列求和
-        SR=(ras_quanted.sum(axis=0))*self.scale
-
-        return SR
+        ras_dequanted=(ras_quanted.sum(axis=0))*self.scale
+        return ras_dequanted
+    
+    def calculate_error(self,weight_matrix=None):
+        SR=self.dequantize()
+        return self.ST-SR
 
 if __name__=="__main__":
-    ras_inst=ras()
+    
     weight_matrix=np.random.randint(0,256,(4096,4096))
-    ras_inst.calculate_ras(weight_matrix)
+    ras_inst=ras(weight_matrix)
+
+    ras_inst.calculate_ras()
     ras_inst.int8_quantize()
-    SR=ras_inst.dequantize(ras_inst.int8_quantize())
+    SR=ras_inst.dequantize()
+    print(ras_inst.calculate_error())
     
     exit()
     weight_matrix=np.arange(32*16).reshape(32,16)
