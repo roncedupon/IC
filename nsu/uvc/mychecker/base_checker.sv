1ed45a2eb576dbb21858c504a6344e269f23da8a
dy <dy>
2026-03-16 09:33:49 +0000

    update

--- nsu/uvc/mychecker/base_checker.sv
+++ nsu/uvc/mychecker/base_checker.sv
@@ -5,7 +5,7 @@ class base_checker #(type T = uvm_object) extends uvm_component;
     super.new(name, parent);
   endfunction
 
-  // 删除队列中与指定队列匹配的项目
+  // Delete items from source queue that match items in delete queue
   function void delete_queue_items(ref T src_que[$], const ref T del_que[$]);
     int del_cnt = 0;
     int idx_que[$];
@@ -26,7 +26,7 @@ class base_checker #(type T = uvm_object) extends uvm_component;
     `uvm_info("DELETE_ITEMS", $sformatf("Deleted %0d matched queue items", del_cnt), UVM_MEDIUM);
   endfunction
 
-  // 查找队列中与指定条件匹配的项目
+  // Find items in queue that match the specified condition
   function void find_queue_items(ref T src_que[$], ref T res_que[$], function bit condition(T item));
     res_que.delete();
     foreach(src_que[i]) begin
@@ -36,14 +36,14 @@ class base_checker #(type T = uvm_object) extends uvm_component;
     end
   endfunction
 
-  // 比较两个事务是否相等
+  // Compare two transactions for equality
   virtual function bit compare_transactions(T t1, T t2);
     if(t1 == null || t2 == null) return 0;
-    // 默认比较引用相等性
+    // Default comparison: reference equality
     return (t1 == t2);
   endfunction
 
-  // 打印队列内容
+  // Print queue content
   function void print_queue(ref T que[$], string queue_name = "queue");
     `uvm_info("QUEUE_CONTENT", $sformatf("=== %s content (%0d items) ===", queue_name, que.size()), UVM_MEDIUM);
     foreach(que[i]) begin
@@ -57,24 +57,24 @@ class base_checker #(type T = uvm_object) extends uvm_component;
     `uvm_info("QUEUE_CONTENT", "==============================", UVM_MEDIUM);
   endfunction
 
-  // 清空队列并释放内存
+  // Clear queue and release memory
   function void clear_queue(ref T que[$]);
     foreach(que[i]) begin
       if(que[i] != null) begin
-        // 这里可以添加释放内存的逻辑，如果需要的话
+        // Memory release logic can be added here if needed
       end
     end
     que.delete();
     `uvm_info("CLEAR_QUEUE", "Queue cleared", UVM_MEDIUM);
   endfunction
 
-  // 按字段值查找事务
+  // Find transactions by field value
   function void find_transactions_by_field(ref T src_que[$], ref T res_que[$], string field_name, uvm_object field_value);
     res_que.delete();
     foreach(src_que[i]) begin
       if(src_que[i] != null) begin
-        // 这里可以根据具体的字段类型实现查找逻辑
-        // 例如：if(src_que[i].field_name == field_value) res_que.push_back(src_que[i]);
+        // Field-based search logic can be implemented here
+        // Example: if(src_que[i].field_name == field_value) res_que.push_back(src_que[i]);
       end
     end
   endfunction
